56639f0f0926148613e50a30ae3d0140ea9b1ecb
s.serubin <s.serubin@yandex.ru>
2025-02-27 13:03:52 +0300

    update

--- test_yolov8.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-# Load the YOLOv8 model
-model = YOLO('yolov8n.pt')  # Make sure to download the model weights if you haven't already
-print("Model loaded successfully!")
